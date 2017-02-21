653cf9b4027c9af500692c045fa6341d9e05fd92
taita2015 <mmix.xia@gmail.com>
2017-02-21 23:18:23 +0800

    0221

--- 5.3.cpp
+++ 5.3.cpp
@@ -4,25 +4,38 @@
 using namespace std;
 class CloseNumber {
 public:
-	int * intToArray(int n){
-						
-	}
-	int arrayToInt(int A[],int len){
-		int res;
-		for (int i = 0; i < len; ++i)
-		{
-			res = (res<<1);
-			res = res|A[i];
-		}
-		return res;
-	}
-
     vector<int> getCloseNumber(int x) {
+    	vector<int> res;
+    	//smaller
+    	int t = x;
+    	int oneCount = 0;
+    	while(t){
+    		if(t&1){
+	    		oneCount++;
+    		}
+    		t=t>>1;
+    	}
+    	t = 0;
+    	for (int i = 0; i < oneCount; ++i)
+    	{
+    		t=t<<1;
+    		t=t|1;
+    	}
+    	res.push_back(t);
 
+
+    	//bigger
+
+    	
+    	return res;
     }
 };
 int main(){
 	CloseNumber c;
-	c.intToArray();	
+	vector<int> res = c.getCloseNumber(5);
+	for (int i = 0; i < res.size(); ++i)
+	{
+		cout<<res[i]<<endl;
+	}
 	return 0;
 }
