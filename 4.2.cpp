c7583d18a9552f11421e639c2883a48ae0468e91
taita2015 <mmix.xia@gmail.com>
2017-02-21 11:40:39 +0800

    0221

--- 4.2.cpp
+++ 4.2.cpp
@@ -19,43 +19,33 @@ public:
     		return false;
     	}
     }
-    vector<UndirectedGraphNode*> l;
-
-    bool visit(UndirectedGraphNode *p){
-    	l.push_back(p);
-    }
-    bool isVisit(UndirectedGraphNode *p){
-    	for (int i = 0; i < l.size(); ++i)
-    	{
-    		if(l[i]==p){
-    			return true;
-    		}else{
-    			return false;
-    		}
-    	}
-    	return false;
-    }
-    bool clearVisit(){
-    	l.clear();
-    }
 
     bool AtoB(UndirectedGraphNode *a,UndirectedGraphNode *b){
-    	clearVisit();
-    	queue<UndirectedGraphNode *> q;
+    	vector<UndirectedGraphNode *> visited;
+    	queue<UndirectedGraphNode *> q;  	
     	q.push(a);
     	while(!q.empty()){
-    		UndirectedGraphNode *p = q.front();
-    		q.pop();
-    		if(isVisit(p)){
-    			continue;
-    		}
-    		if(p==b){
-    			return true;
-    		}
-    		visit(p);
-    		for (int i = 0; i < (p->neighbors).size(); ++i)
+    		UndirectedGraphNode *p = q.front();q.pop();
+    		bool isVisit = false;
+    		for (int i = 0; i < visit.size(); ++i)
     		{
-    			q.push((p->neighbors)[i]);
+    			if(visited[i]==p){
+    				isVisit = true;
+    				break;
+    			}
+    		}
+    		if(isVisit){
+    			continue;
+    		}else{
+	    		if(p==b){
+    				return true;
+	    		}else{
+		    		for (int i = 0; i < (p->neighbors).size(); ++i)
+		    		{
+		    			q.push((p->neighbors)[i]);
+		    		}
+	    		}
+
     		}
     	}
     	return false;
