d7a11a60b9858c830bf7991e4f99471d5ae73705
taita2015 <mmix.xia@gmail.com>
2017-02-22 16:04:12 +0800

    0222

---
+++ 7.6.fix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+    Point() :
+            x(0), y(0) {
+    }
+    Point(int xx, int yy) {
+        x = xx;
+        y = yy;
+    }
+};
+class DenseLine {
+public:
+    vector<double> getLine(vector<Point> p, int n) {
+    	
+    }
+};
+
+int main(){
+
+
+	return 0;
+}
---
+++ 7.7.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+class KthNumber {
+public:
+    int findKth(int k) {
+        // write code here
+    }
+};
+
+int main(){
+	
+	return 0;
+}
