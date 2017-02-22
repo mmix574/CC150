7b7dfcddf51b527a1ff5d84ed2bd940b725052a3
taita2015 <mmix.xia@gmail.com>
2017-02-22 20:54:47 +0800

    0222

---
+++ 9.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class GoUpstairs {
+public:
+	
+	
+    int countWays(int n) {
+    	if(n==1){
+    		return 1;
+    	}else if(n==2){
+    		return 2;
+    	}else if(n==3){
+    		return 4;
+    	}else{
+    		return countWays(n-1)+countWays(n-2)+countWays(n-3);
+    	}
+    }
+};
+
+int main(){
+	return 0;
+}
