83b17412f31c29562c120d8be2d6892963885ca7
taita2015 <mmix.xia@gmail.com>
2017-03-09 20:23:40 +0800

    0300

--- 17.4.cpp
+++ 17.4.cpp
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 class Max {
 public:
     int getMax(int a, int b) {
+    	int A[2];
+    	A[0] = a;
+    	A[1] = b;
+    	int c = 0;
+    	int t = a-b>0||c++;
+    	return A[c];
     }
 };
+
+int main(){
+	Max m;
+	printf("%d\n",m.getMax(2,1));
+	return 0;
+}
