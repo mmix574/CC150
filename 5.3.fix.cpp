59b8ebcde3adc59a1494d2cdd73447f6efd037f9
taita2015 <mmix.xia@gmail.com>
2017-03-09 19:55:18 +0800

    0309

--- 5.3.fix.cpp
+++ 5.3.fix.cpp
@@ -1,64 +1,72 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 class CloseNumber {
 public:
+	int get(int x,int p){
+		return (x>>p)&1;
+	}
+	int swap(int x,int p){
+		if((x>>p)&1){
+			int c = 1;
+			c = ~(c<<p);
+			return x&c;
+		}else{
+			int c =1;
+			c = c<<p;
+			return x|c;
+		}
+	}
     vector<int> getCloseNumber(int x) {
     	vector<int> res;
+    	int i = 0;
+    	// bigger
+    	while(!get(x,i)){i++;}
+    	int first_one = i; 
+    	while(get(x,i)){i++;}
+    	int first_zero = i;
+    	int bigger = x;
+    	bigger = swap(bigger,first_zero);
+    	bigger = swap(bigger,first_one);
+
+    	// printf("%d\n",first_one);
+    	// printf("%d\n",first_zero);
+    	// printf("%d\n",bigger);
     	//smaller
-    	int t = x;
-    	int c = 0x7FFFFFFE;
-    	int b = 0x00000001;
-    	
-
-    	int firstZero=0;
-    	while((t>>firstZero)&1){
-    		firstZero++;
+    	i = 0;
+    	while(get(x,i)){
+    		i++;
     	}
-    	int firstOnePassZero = firstZero;
-    	while(!((t>>firstOnePassZero)&1)){
-    		firstOnePassZero++;
+    	first_zero = i;
+    	while(!get(x,i)){
+    		i++;
     	}
-    	t=t|(b<<firstZero);
-    	for (int i = 0; i < firstOnePassZero; ++i)
-    	{
-    		c=c<<1;
-    		c=c|1;
+    	first_one = i;
+    	int smaller = x;
+    	smaller=swap(smaller,first_one);
+    	smaller=swap(smaller,first_one-1);
+    	i = i-2;
+    	int j = 0;
+    	if(!get(x,i)){
+    		smaller=swap(smaller,i--);
+    		smaller=swap(smaller,j++);
     	}
-    	t=t&c;
-    	res.push_back(t);
-
-
-    	//bigger
-    	t = x;
-    	int firstOne = 0;
-    	while(!((x>>firstOne)&1)){
-    		firstOne++;
-    	}
-    	int firstZeroPassOne = firstOne;
-    	while((x>>firstZeroPassOne)&1){
-    		firstZeroPassOne++;
-    	}
-    	t=(t>>firstZeroPassOne)|1;
-    	t=(t<<firstZeroPassOne)|x;
-    	c = 0x7FFFFFFE;
-    	for (int i = 0; i < firstOne; ++i)
-    	{
-    		c=(c<<1);
-    		c=c|1;
-    	}
-    	t=t&c;
-    	res.push_back(t);    	
+    	res.push_back(smaller);
+    	res.push_back(bigger);
     	return res;
     }
 };
 int main(){
 	CloseNumber c;
-	vector<int> res = c.getCloseNumber(2);
+	vector<int> res = c.getCloseNumber(76351);
 	for (int i = 0; i < res.size(); ++i)
 	{
 		cout<<res[i]<<endl;
 	}
 	return 0;
+//76351
+// 对应输出应该为:
+// [76284,76383]
 }
