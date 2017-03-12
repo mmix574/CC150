931ea2c94e67d6b0b3c90af31f49a91d89a4c0eb
taita2015 <mmix.xia@gmail.com>
2017-03-12 13:35:52 +0800

    0312

--- 9.1_lasting.cpp
+++ 9.1_lasting.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 class GoUpstairs {
 public:
-	int buf[100000] = {-1};
+	int buf[100000] = {0};
     int countWays(int n) {
     	int a,b,c;
-    	if(n==1){
+    	if(n<=0){
+    		return 0;
+    	}else if(n==1){
     		buf[1] = 1;
     		return 1;
     	}else if(n==2){
@@ -16,28 +18,29 @@ public:
     		buf[3] = 4;
     		return 4;
     	}else{
-    		if(buf[n-1]!=-1){
+    		if(buf[n-1]){
     			a = buf[n-1];
     		}else{
-    			a = buf[n-1] = countWays(n-1); 
+    			a = buf[n-1] = (countWays(n-1))%1000000007; 
     		}
-    		if(buf[n-2]!=-1){
+    		if(buf[n-2]){
     			b = buf[n-2];
     		}else{
-    			b = buf[n-2] = countWays(n-2); 
+    			b = buf[n-2] = (countWays(n-2))%1000000007;
+
     		}
-    		if(buf[n-3]!=-1){
+    		if(buf[n-3]){
     			c = buf[n-3];
     		}else{
-    			c = buf[n-3] = countWays(n-3); 
+    			c = buf[n-3] = (countWays(n-3))%1000000007; 
     		}
-    		return a+b+c;
+    		return (a+b+c)%1000000007;
     	}
     }
 };
 
 int main(){
 	GoUpstairs g;
-	cout<<g.countWays(3)<<endl;
+	g.countWays(36196);
 	return 0;
 }
